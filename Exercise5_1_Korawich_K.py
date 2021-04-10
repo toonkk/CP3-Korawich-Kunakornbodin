60288fc62af52baf2f2e24d25ead9299c87eeef8
toonkk <82014476+toonkk@users.noreply.github.com>
2021-04-10 12:03:02 +0700

    Create Exercise5_1_Korawich_K.py

---
+++ Exercise5_1_Korawich_K.py
@@ -0,0 +1,12 @@
+a = int(input("The first number :",))
+b = int(input("The second number :",))
+r1 = a+b
+r2 = a-b
+r3 = a*b
+r4 = a/b
+print("-------Result-------")
+print("Result of a+b: ", r1)
+print("Result of a-b: ", r2)
+print("Result of a*b: ", r3)
+print("Result of a/b: ", r4)
+print("--------End--------")
